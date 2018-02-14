1a377b62b8e96c1a5970a6726b8d426ecf380b50
Pavlo Volosianskyi <a.lorreon@gmail.com>
2018-02-14 12:33:23 +0200

    ajax_1

---
+++ www/my.js
@@ -0,0 +1,24 @@
+$("#information").hide();
+
+function funcBefore() {
+    $("#information").toggle().text("Ожидание данных...");
+}
+
+function funcSuccess(data) {
+    $("#information").text(data);
+}
+
+$(document).ready(function () {
+    $("#load").bind("click", function () {
+        var admin = "Admin";
+        $.ajax({
+            url: "content.php",
+            type: "POST",
+            data: ({name: admin, number: 5}),
+            dataType: "html",
+            beforeSend: funcBefore,
+            success: funcSuccess
+        });
+    });
+});
+
